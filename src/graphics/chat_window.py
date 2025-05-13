017cb9d81ef845660c62a362e58acfc5c347de7a
Yejin715 <715jin@naver.com>
2025-05-13 16:21:37 +0900

    250513-Added select button between Whisper and Keyboard input methods

--- src/graphics/chat_window.py
+++ src/graphics/chat_window.py
@@ -1,16 +1,16 @@
 import dearpygui.dearpygui as dpg
 
-from ..message_event import MessageListener, MessageBroker, MessageType
-from ..async_event import AsyncListener, AsyncBroker, AsyncMessageType
+from ..message_event import MessageBroker, MessageType
+from ..async_event import AsyncBroker, AsyncMessageType
 
 class ChatWindow:
     _window: any
-    _width: int
+    _width: int 
     _height: int
+    use_whisper =  False  # 기본은 Whisper 사용 (현재는 꺼놓은 상황)
 
     def __init__(self):
-        self._msg_broker = MessageBroker()
-        self._async_broker = AsyncBroker()
+        pass
 
     def setup(self, width: int, height: int, title_bar: bool) -> "ChatWindow":
         # Setup the window
@@ -18,14 +18,18 @@ class ChatWindow:
             self._window = w
             self._width = width
             self._height = height
+            
             dpg.add_text("대화 내용")
             # 고정 크기의 채팅 기록 영역을 child_window로 생성
             CHAT_AREA_HEIGHT = int(height * 0.3)  # 예: 전체 창 높이의 60% 정도로 설정
             with dpg.child_window(width=width - 20, height=CHAT_AREA_HEIGHT, border=True) as chat_area:
                 self._chat_area = chat_area  # 여기서 저장해둠
                 self._el_loading = dpg.add_loading_indicator()
+                self._el_recording_label = dpg.add_text("Recording...", show=False)
+                self._el_recording_indicator = dpg.add_loading_indicator(show=False)
                 # 내용이 길어지면 기본적으로 스크롤바가 생깁니다.
-            
+            dpg.add_checkbox(label="음성인식사용여부 (미사용시 키보드 입력)", default_value=False, callback=self._on_toggle_whisper)
+
             # 입력 영역 (라벨과 입력창)
             with dpg.group(horizontal=True):
                 dpg.add_text("입력 : ")
@@ -33,8 +37,6 @@ class ChatWindow:
             dpg.add_button(label="Send", callback=self._on_send)
             dpg.add_button(label="Wake up", callback=self._on_wakeup_btn)
             dpg.add_button(label="Stop conversation", callback=self._on_stop)
-            self._el_recording_label = dpg.add_text("Recording...", show=False)
-            self._el_recording_indicator = dpg.add_loading_indicator(show=False)
 
             # Subscribe event
             AsyncBroker().subscribe("chat_response", self._on_chat_response)
@@ -77,8 +79,11 @@ class ChatWindow:
         AsyncBroker().emit(("wake_up", None))
 
     def _on_stop(self):
-        dpg.configure_item(self._el_loading, show=True)
         AsyncBroker().emit(("chat_done", None))
+
+    def _on_toggle_whisper(self, sender, app_data, user_data=None):
+        ChatWindow.use_whisper = app_data  # True면 Whisper 사용, False면 키보드 입력
+        print("Whisper mode:", ChatWindow.use_whisper)
         
     # External Callbacks
     def _on_chat_response(self, msg: tuple[str, str]):
@@ -88,8 +93,10 @@ class ChatWindow:
 
         if response.get('type') in [None, "text", "meta"]:
             self._add_bot_msg(response['msg'])
+            AsyncBroker().emit(("chat_listening_start", None))
         elif response.get('type') == "music-card":
             self._add_bot_msg(f"Playing {response['msg']['src']} ...")
+            AsyncBroker().emit(("chat_listening_start", None))
         else:
             print(f"Unknown response type {response['type']}")
 
@@ -106,7 +113,6 @@ class ChatWindow:
         dpg.configure_item(self._el_recording_label, show=False)
         dpg.configure_item(self._el_recording_indicator, show=False)
         dpg.configure_item(self._el_loading, show=False)
-        # self._add_bot_msg("(대화 마침.)")
         AsyncBroker().emit(("chat_done_listening", None))
 
     def _on_chat_done_listening(self, msg: MessageType):
